7ea2707b899381b0dbc5e2f651cd5896ff0bdb6a
JJ Merelo <jjmerelo@gmail.com>
2023-05-19 17:38:12 +0200

    Changes final names closes #1

--- bin/commit-push.sh
+++ bin/commit-push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 git config --global user.email "jjmerelo@gmail.com"
-git config --global user.name "NovaMLOpsBot"
+git config --global user.name "VeneciaRedesBot"
 git commit -am "[🤖] $1"
 
