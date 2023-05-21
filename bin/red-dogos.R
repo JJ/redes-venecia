eb4f1298666f05dcd1413ca31db0f5d11f12ed16
JJ Merelo <jjmerelo@gmail.com>
2023-05-21 14:18:16 +0200

    Reconstruido el cubo, con tamaño correcto
    Closes #7 closes #8 porque ya está todo incluido

--- bin/red-dogos.R
+++ bin/red-dogos.R
@@ -17,9 +17,10 @@ for (i in 1:3) {
   main_ids <- V(doges.main.sn)[degree(doges.main.sn) > 1 ]
   doges.main.sn <- igraph::induced_subgraph(doges.main.sn, main_ids)
 }
+
 plot(doges.main.sn,
      vertex.size=V(doges.main.sn)$degree,
-     layout=layout_with_fr,
+     layout=layout_nicely,
      vertex.label.cex=V(doges.main.sn)$EV*2,
      vertex.label.dist=1,
      edge.width=3*E(doges.main.sn)$weight)
