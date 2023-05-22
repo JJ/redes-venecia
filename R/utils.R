aab0725542d6cf8a9db18e1619e3013ce42a9ad2
JJ Merelo <jjmerelo@gmail.com>
2023-05-22 09:07:29 +0200

    :memo: closes #10 porque realmente ya se están usando los pesos

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@ doges.sn <- marriage.graph()
 E(doges.sn)$weight <- 1
 doges.sn <- simplify(doges.sn, edge.attr.comb=list(weight="sum"))
 V(doges.sn)$degree <- degree(doges.sn)
-V(doges.sn)$EV <- unname(eigen_centrality(doges.sn)$vector)
+V(doges.sn)$EV <- unname(eigen_centrality(doges.sn)$vector) # unname extrae solo el vector de valores; los pesos se usan implícitamente
 components <- igraph::clusters(doges.sn, mode="weak")
 biggest_cluster_id <- which.max(components$csize)
 vert_ids <- V(doges.sn)[components$membership == biggest_cluster_id]
