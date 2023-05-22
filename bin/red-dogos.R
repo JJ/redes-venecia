8759077e5d3ffb321e48b47d4ffdfed5e2a60d43
JJ Merelo <jjmerelo@gmail.com>
2023-05-22 08:51:00 +0200

    :sparkles: Usando source para definir variables directamente
    Refs #9

--- bin/red-dogos.R
+++ bin/red-dogos.R
@@ -1,15 +1,7 @@
-library(dogesr)
 library(igraph)
-doges.sn <- marriage.graph()
-E(doges.sn)$weight <- 1
-doges.sn <- simplify(doges.sn, edge.attr.comb=list(weight="sum"))
-V(doges.sn)$degree <- degree(doges.sn)
-V(doges.sn)$EV <- unname(eigen_centrality(doges.sn)$vector)
-components <- igraph::clusters(doges.sn, mode="weak")
-biggest_cluster_id <- which.max(components$csize)
-vert_ids <- V(doges.sn)[components$membership == biggest_cluster_id]
 
-doges.sn.connected <- igraph::induced_subgraph(doges.sn, vert_ids)
+source("../R/utils.R")
+
 main_ids <- V(doges.sn.connected)[degree(doges.sn.connected) > 1 ]
 doges.main.sn <- igraph::induced_subgraph(doges.sn.connected, main_ids)
 
