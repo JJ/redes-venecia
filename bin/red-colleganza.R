a3c09a1a70b46b7f1f05fa82b3783dccacee24f1
JJ Merelo <jjmerelo@gmail.com>
2023-05-25 07:38:03 +0200

    Mejor representación de la red de colleganza #13 #16

--- bin/red-colleganza.R
+++ bin/red-colleganza.R
@@ -1,6 +1,7 @@
 library(igraph)
 library(qgraph)
 library(dogesr)
+library(dplyr)
 
 colleganza <- read.csv("../data/colleganza-pairs.csv", header=F)
 colleganza.sn <- graph.data.frame(data.frame(colleganza$V1,colleganza$V2),directed=F)
@@ -16,15 +17,19 @@ vert_ids <- V(colleganza.sn)[components$membership == biggest_cluster_id]
 colleganza.sn.connected <- igraph::induced_subgraph(colleganza.sn, vert_ids)
 
 # Traza para a4
-rojo.dogo <- rgb(0.7,0,0,0.6)
-V(colleganza.sn.connected)$color="blue"
+rojo.dogo <- rgb(0.7,0,0,0.8)
+V(colleganza.sn.connected)$color=rgb(0,0,1,0.5)
+
+doge.families <- data.doges %>% count(Family.doge,sort=T)
+V(colleganza.sn.connected)[ V(colleganza.sn.connected)$name %in% doge.families$Family.doge ]$color <- rojo.dogo
 edges <- get.edgelist(colleganza.sn.connected,names=FALSE)
 colleganza.layout  <- qgraph.layout.fruchtermanreingold(edges,vcount=vcount(colleganza.sn.connected))
 plot(colleganza.sn.connected,
      vertex.size=V(colleganza.sn.connected)$degree/2,
      layout=colleganza.layout,
-     vertex.label.cex=1+V(colleganza.sn.connected)$betweenness/400,
+     vertex.label.cex=1+V(colleganza.sn.connected)$betweenness/2000,
      vertex.label.dist=0.5,
+     vertex.label.color=V(colleganza.sn.connected)$color,
      edge.width=3*E(colleganza.sn.connected)$weight)
 
 # Elimina colgantes
