e651cee2c40f328c00b495defb56db6029768257
JJ Merelo <jjmerelo@gmail.com>
2023-08-31 11:49:32 +0200

    Extracts scripts

---
+++ ev-centrality.R
@@ -0,0 +1,59 @@
+## ----setup, echo=F,message=F-----------------------------------------------------------------------------
+library(igraph)
+library(knitr)
+library(stringr)
+library(kableExtra)
+tablify <- function( graph, column ) {
+  m.table <- data.frame( Family=V(graph)$name, degree=vertex_attr(graph,column) )
+  names(m.table)[names(m.table) == "degree"] <- column
+  return( m.table[ order(m.table[[column]],decreasing = T),] )
+}
+
+
+load("data/venice-marriages.Rda")
+marriages <- marriages.raw[ marriages.raw$wife_familyname_std != '',] # Eliminates those that are not noble
+
+# Use self-loops
+all.marriages <- marriages                                    # Keep marriages with intra-family ties
+all.marriages.sn <- graph.data.frame(data.frame(all.marriages$husband_familyname_std,all.marriages$wife_familyname_std),directed=F)
+V(all.marriages.sn)$eigen <-  unname(unlist(eigen_centrality(all.marriages.sn)$vector))
+all.m.eigen <- tablify( all.marriages.sn, "eigen")
+top.ev.all.eigen <- all.m.eigen %>% head(.,10)
+
+# Eliminate self-loops
+marriages <- marriages[ marriages$husband_familyname_std != marriages$wife_familyname_std,]
+marriages.sn <- graph.data.frame(data.frame(marriages$husband_familyname_std,marriages$wife_familyname_std),directed=F)
+V(marriages.sn)$eigen <-  unname(unlist(eigen_centrality(marriages.sn)$vector))
+m.eigen <- tablify( marriages.sn, "eigen")
+top.ev.eigen <- m.eigen %>% head(.,10)
+
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(ggplot2)
+library(ggthemes)
+
+marriages.self <- marriages.raw[ marriages.raw$husband_familyname_std == marriages.raw$wife_familyname_std,]
+sorted.marriages.self <- marriages.self %>% count(wife_familyname_std, sort=T)
+intra.marriages <- unique(marriages.self$wife_familyname_std)
+
+top.intra.marriages <-  sorted.marriages.self %>% head(.,10)
+
+tabled.top.eigen <- data.frame(Family.Intra = top.intra.marriages$wife_familyname_std, Marriages=top.intra.marriages$n, Family = top.ev.eigen$Family, EV = top.ev.eigen$eigen, Family.Self=top.ev.all.eigen$Family, EV.self = top.ev.all.eigen$eigen)
+kable(tabled.top.eigen,
+      col.names=c("Family", "#Intra-family matrimonies", "Family", "EV","Family","EV + self-loops" ),
+      caption="Ranking of families according to number of intra-family matrimonies (left) and EV, with (right) or without (center) self-loops. \\protect\\label{tab:top:ev}",
+      row.names = F, booktabs = T) %>% kable_styling(latex_options = c("striped")) %>% column_spec(2, border_right=T) %>% column_spec(3, border_left = T ) %>% column_spec(4, border_right=T) %>% column_spec(5, border_left = T )
+
+
+## ----charts, echo=F, fig.show="hold", out.width="50%",message=F, fig.cap="(Left) EV centrality considering self-loops (x axis) or not (y axis). The size of the plot and color signal the number of intra-family marriages. (Right) Number of intra-family marriages vs. percent increment of EV when self-loops are not computed. \\protect\\label{fig:plots}"----
+all.names <- unique( c( marriages.raw$wife_familyname_std, marriages.raw$husband_familyname_std))
+all.marriages <- marriages.raw %>% select(husband_familyname_std,wife_familyname_std) %>% rowid_to_column() %>% pivot_longer(-rowid) %>% group_by(value) %>% summarise(n = n_distinct(rowid)) %>% arrange(desc(n))
+marriages.table <- left_join(all.marriages,sorted.marriages.self,by=c("value" = "wife_familyname_std"))
+marriages.table$intra.marriages.rate <- marriages.table$n.y / marriages.table$n.x
+
+marriages.summary <- data.frame(family=intra.marriages, EV.with.self=sapply( intra.marriages, function(x) V(all.marriages.sn)[[x]]$eigen ), EV.no.self=sapply( intra.marriages, function(x) V(marriages.sn)[[x]]$eigen ), intra.marriages=sapply( intra.marriages, function(x) sorted.marriages.self[sorted.marriages.self$wife_familyname_std ==x,]$n ))
+ggplot(marriages.summary, aes(x=EV.with.self,y=EV.no.self, color=intra.marriages, size=intra.marriages))+geom_point()+geom_abline(intercept=0,slope=1,linetype="dashed",color="red")+scale_x_log10()+scale_y_log10()
+marriages.summary$diff <- (marriages.summary$EV.no.self - marriages.summary$EV.with.self)/marriages.summary$EV.no.self
+ggplot(marriages.summary, aes(x=intra.marriages,y=diff))+geom_point()+theme_fivethirtyeight()
+
