eeaf913a091ea858601f49850009d1864b263f75
JJ Merelo <jjmerelo@gmail.com>
2023-11-01 12:15:30 +0100

    #21 corresponding script

--- R/process-freight-data.R
+++ R/process-freight-data.R
@@ -1,1 +1,9 @@
-freight.traffic <- read.csv("data/freight-US.csv", header = TRUE, sep = ",")
+library(dplyr)
+freight.traffic <- read.csv("data/freight-US.csv", header = TRUE, sep = ";")
+
+freight.traffic$freight <- as.numeric(gsub(",", ".", freight.traffic$Valores.de.medidas))
+freight.traffic %>% 
+  group_by(Origin.State, Dest.State, Year) %>% 
+  summarise(sum = sum(freight)) -> freight.traffic.agg
+save(freight.traffic.agg, file = "data/freight-traffic-agg.RData")   
+  
