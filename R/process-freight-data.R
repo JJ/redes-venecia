92e26ae5500ea459e8ef629e40729d39193558e5
JJ Merelo <jjmerelo@gmail.com>
2023-11-01 12:55:11 +0100

    No need to simplify edges, it eliminates self-loops

--- R/process-freight-data.R
+++ R/process-freight-data.R
@@ -5,5 +5,6 @@ freight.traffic$freight <- as.numeric(gsub(",", ".", freight.traffic$Valores.de.
 freight.traffic %>% 
   group_by(Origin.State, Dest.State, Year) %>% 
   summarise(sum = sum(freight)) -> freight.traffic.agg
+
 save(freight.traffic.agg, file = "data/freight-traffic-agg.RData")   
   
