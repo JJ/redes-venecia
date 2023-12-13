25f02f4a3c451f0fe9a1bcfcad05c66542611154
JJ Merelo <jjmerelo@gmail.com>
2023-12-13 20:37:51 +0100

    #30 analyzing entropy and number of families

--- R/change-point-marriages.R
+++ R/change-point-marriages.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(trend)
 library(anomalize)
 library(tibble)
+library(DescTools)
 
 load("data/venice-marriages.Rda")
 
@@ -28,3 +29,10 @@ marriage.t <- marriage.t %>% tibbletime::as_tbl_time(index = date)
 marriage.t %>% time_decompose(value) %>% anomalize(remainder) %>% time_recompose() -> marriage.decomposed
 
 marriage.decomposed %>% plot_anomalies(time_recomposed = TRUE, ncol = 3, alpha_dots = 0.5) + theme_minimal() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + ggtitle("Marriages by year")
+
+marriages %>% group_by(year) %>% summarise( families = length(unique( c(unique(husband_familyname_std,unique(wife_familyname_std)) ) ) )) -> married.families.by.year
+
+marriages %>% group_by(year) %>% summarise( norm.entropy = (unique( c(unique(husband_familyname_std,unique(wife_familyname_std)) ) ) )) -> married.families.by.year
+
+# merge married.families.by.year and marriages.by.year
+marriage.data.by.year <- merge(married.families.by.year,marriages.by.year,by="year")
